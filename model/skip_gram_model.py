c4fa0a67be6dab39e4e94c4c84c19dd067979b47
JoneySun <joneysun@foxmail.com>
2022-01-20 19:58:36 +0800

    add SkipGramModel

--- model/skip_gram_model.py
+++ model/skip_gram_model.py
@@ -13,3 +13,24 @@ class SkipGramModel(nn.Module):
         self.output_emb = nn.Embedding(vocab_size, embedding_size)
         self.input_emb.weight.data.uniform_(-1, 1)
         self.output_emb.weight.data.uniform_(-1, 1)
+        # initrange = 0.5 / self.embedding_size
+        # self.input_emb.weight.data.uniform_(-initrange, initrange)
+        # self.output_emb.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, input, pos_labels, neg_labels):
+        # input_labels:[batch_size]
+        # pos_labels:[batch_size, windows_size*2]
+        # neg_labels:[batch_size, windows_size * N_SAMPLES]
+        input = self.input_emb(input)
+        pos = self.output_emb(pos_labels)
+        neg = self.output_emb(neg_labels)
+
+        input = input.unsqueeze(2)
+        pos_dot = torch.bmm(pos, input).squeeze(2)
+        neg_dot = torch.bmm(neg, input).squeeze(2)
+
+        log_pos = F.logsigmoid(pos_dot).sum(1)
+        log_neg = F.logsigmoid(neg_dot).sum(1)
+
+        loss = -(log_pos + log_neg)
+        return loss
