75de581134a6a0292a58f5bb4bba4b434ee0a56a
JoneySun <1781825241@qq.com>
2022-04-15 19:01:31 +0800

    fix string ==' ' bug

--- transform/base_tokenizer.py
+++ transform/base_tokenizer.py
@@ -9,7 +9,9 @@ class BaseTokenizer:
         self.tokenizer = MiNLPTokenizer(granularity=granularity)
 
     def __call__(self, data):
-        if isinstance(data, list):
-            data = list(map(lambda x: x if x != '' else ' ', data))
-        data = self.tokenizer.cut(data)
+        if not isinstance(data, list):
+            return self.tokenizer.cut(data)
+        data1 = list(map(lambda x: x if x.strip() != '' else '#', data))
+        data1 = self.tokenizer.cut(data1)
+        data = list(map(lambda x: [''] if len(x) == 1 and x[0] == '#' else x, data1))
         return data
